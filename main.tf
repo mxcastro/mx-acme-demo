dc99eb7a2fe16d5e2eaae58248f580079c52de34
Maxwell Castro <maxwellsullivan@gmail.com>
2025-07-29 18:55:52 -0300

    update deploy script

--- main.tf
+++ main.tf
@@ -102,9 +102,10 @@ resource "aws_instance" "webapp_instance" {
 
   # User data script to configure the instance on launch
   # Reads the script content from a file in the 'files' directory
-  user_data = templatefile("${path.module}/deploy_app.sh", {
-    github_repo_url = var.github_repo_url # Only pass the variable that the script expects
-  })
+  # user_data = templatefile("${path.module}/deploy_app.sh", {
+  #   github_repo_url = var.github_repo_url # Only pass the variable that the script expects
+  # })
+  user_data = file("${path.module}/deploy_app.sh")
 
   tags = {
     Name = "acme-webapp-instance"
