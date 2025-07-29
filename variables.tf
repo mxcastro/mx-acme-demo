34ab4a99b1d60c3b6e2dc65712594a6449b7ead9
Maxwell Castro <59934897+mxcastro@users.noreply.github.com>
2025-07-29 02:45:42 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,13 +31,3 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "aws_access_key_id" {
-  description = "AWS Access Key ID"
-  type        = string
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true 
-}
