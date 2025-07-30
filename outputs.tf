75a8d2fce02decc8fadb3ac50506580fa67b97e8
Maxwell Castro <maxwellsullivan@gmail.com>
2025-07-29 22:28:46 -0300

    forgot to save adjustment in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 # Output the ID of the created S3 bucket
 output "s3_bucket_id" {
   description = "The ID (name) of the S3 bucket."
-  value       = module.my_s3_bucket.bucket_id
+  value       = module.acme_s3_bucket.bucket_id
 }
 
 # Output the ARN of the created S3 bucket
 output "s3_bucket_arn" {
   description = "The ARN of the S3 bucket."
-  value       = module.my_s3_bucket.bucket_arn
+  value       = module.acme_s3_bucket.bucket_arn
 }
