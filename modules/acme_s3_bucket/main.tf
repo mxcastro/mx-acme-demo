e2b88199ff2c0051ec406358010f5b6abb772957
Maxwell Castro <maxwellsullivan@gmail.com>
2025-07-30 01:25:36 -0300

    set public access

--- modules/acme_s3_bucket/main.tf
+++ modules/acme_s3_bucket/main.tf
@@ -4,6 +4,15 @@ resource "aws_s3_bucket" "this" {
   tags   = var.common_tags
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_ownership_controls" "this" {
   bucket = aws_s3_bucket.this.id
 
@@ -92,13 +101,4 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
 
     status = "Enabled"
   }
-}
-
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = false
 }
