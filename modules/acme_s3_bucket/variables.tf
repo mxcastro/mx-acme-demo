7fdea65e4d26991c51a59e4c5513cc16c12d5bed
Maxwell Castro <maxwellsullivan@gmail.com>
2025-07-29 22:37:29 -0300

    adjust tags

--- modules/acme_s3_bucket/variables.tf
+++ modules/acme_s3_bucket/variables.tf
@@ -8,8 +8,5 @@ variable "bucket_name" {
 variable "common_tags" {
   description = "A map of common tags to apply to all resources."
   type        = map(string)
-  default = merge({
-    ManagedBy = "ACME"
-    Owner     = "HCP Terraform Expert Team"
-  })
+  default = {}
 }
