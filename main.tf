68a3f471ce3761f4040436acfa82bbf5d24033e6
Maxwell Castro <maxwellsullivan@gmail.com>
2025-08-03 21:03:22 -0300

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
 module "acme-networking" {
   source            = "app.terraform.io/mx-acme-demo/acme-networking/aws"
